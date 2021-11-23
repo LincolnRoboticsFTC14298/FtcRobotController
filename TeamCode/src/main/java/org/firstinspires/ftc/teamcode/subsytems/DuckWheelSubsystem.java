f07e91418786d2ea55c164815a9f34bf522e1c29
Bryan <bryang7332@gmail.com>
2021-11-22 16:26:58 -0800

    Added DuckWheelSubsystem.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsytems/DuckWheelSubsystem.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.subsytems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+
+/**
+ * Turns on and set the motor at a certain speed so is able to move the duck wheel
+ * @author Bryan
+ */
+class DuckWheelSubsystem extends SubsystemBase {
+
+    /**
+     * Turns on the duck wheel
+     */
+    public void turnOn() {
+
+    }
+    /**
+     * Turns off the duck wheel
+     */
+    public void turnOff() {
+
+    }
+
+}
