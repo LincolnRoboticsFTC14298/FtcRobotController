106d9bf8ec572c4d2aea1af9526e32cf4b407ae3
Jared Haertel <jared.haertel@gmail.com>
2021-11-22 16:47:26 -0800

    Added IntakeSubsystem.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsytems/IntakeSubsystem.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.subsytems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+
+/**
+ * I want the code to turn on and turn off the intake as well as extend the intake and allow
+ * a joystick to change the distance the intake extends.
+ * @author Alex
+ */
+public class IntakeSubsystem extends SubsystemBase {
+
+    /**
+     * Turning on the intake.
+     */
+    public void turnOn() {
+
+    }
+
+    /**
+     * Turns off the intake.
+     */
+    public void  turnOff() {
+
+    }
+
+    /**
+     * Intake extending to a certain distance.
+     */
+    public void extendToDistance(double dist) {
+
+    }
+
+    /**
+     * The range of the intake is controlled by a joystick.
+     */
+    public void   extendJoystick(double x) {
+
+    }
+
+
+}
