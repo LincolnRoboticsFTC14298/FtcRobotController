4ad208c41d470c4a10fc2157c9f3059fa013f0de
LincolnRoboticsFTC14298 <ftc19298@gmail.com>
2022-02-12 12:57:38 -0800

    Uploaded drivebase code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/mecanum/MecanumDriveCommand.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.commands.mecanum;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import org.firstinspires.ftc.teamcode.subsystems.mecanum.MecanumDriveSubsystem;
+
+import java.util.function.DoubleSupplier;
+
+public class MecanumDriveCommand extends CommandBase {
+
+    private final MecanumDriveSubsystem drive;
+    private final DoubleSupplier leftY, leftX, rightX;
+
+    public MecanumDriveCommand(MecanumDriveSubsystem drive, DoubleSupplier leftY,
+                               DoubleSupplier leftX, DoubleSupplier rightX) {
+        this.drive = drive;
+        this.leftX = leftX;
+        this.leftY = leftY;
+        this.rightX = rightX;
+
+        addRequirements(drive);
+    }
+
+    @Override
+    public void execute() {
+        drive.drive(leftY.getAsDouble(), leftX.getAsDouble(), rightX.getAsDouble());
+    }
+
+}
